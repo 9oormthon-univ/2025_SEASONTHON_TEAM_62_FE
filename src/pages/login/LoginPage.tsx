351c7044f8d5e5dc066b75a318ec13ec3b4e52b4
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-09-03 22:33:19 +0900

    feat: 색상토큰 추가, 폰트적용

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <div className="bg-main1 text-safe">로그인 페이지</div>;
+}
--- src/pages/onboarding/OnboardingPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function OnBoardingPage() {
-  return <h1>온보딩 페이지</h1>;
-}
