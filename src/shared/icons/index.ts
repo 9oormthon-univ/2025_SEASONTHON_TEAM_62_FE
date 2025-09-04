ba11837a764e633efacc75ced8b7933484868144
winchoose <tmdxor741@naver.com>
2025-09-04 12:15:04 +0900

    feat: bottomNavition 완료

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -2,5 +2,7 @@
 // Run `pnpm svgr` to regenerate.
 
 export { default as IcSvgMate } from './ic_mate';
+export { default as IcSvgMateFull } from './ic_matefull';
 export { default as IcSvgMyinfo } from './ic_myinfo';
+export { default as IcSvgMyinfoFull } from './ic_myinfofull';
 export { default as IcSvgShoes } from './ic_shoes';
